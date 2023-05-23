b0e69ae8798b12dbd3b3287a7af24a3984bed0b4
robbryandev <robbryandev@gmail.com>
2023-05-23 12:40:31 -0700

    Add base java spring project

---
+++ src/main/java/dev/robbryan/compress/CompressApplication.java
@@ -0,0 +1,13 @@
+package dev.robbryan.compress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/robbryan/compress/CompressApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.robbryan.compress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

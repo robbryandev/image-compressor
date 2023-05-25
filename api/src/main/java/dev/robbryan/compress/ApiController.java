f5a3bb20817fb9794421aa3ef05c6ce13f8c8127
robbryandev <robbryandev@gmail.com>
2023-05-25 14:58:20 -0700

    Add File storage for api route /upload

--- api/src/main/java/dev/robbryan/compress/ApiController.java
+++ api/src/main/java/dev/robbryan/compress/ApiController.java
@@ -3,6 +3,8 @@ package dev.robbryan.compress;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-  public static ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper mapper = new ObjectMapper();
 
-  @GetMapping(path = "/test")
-  public ObjectNode test() {
+  @Autowired
+  private ResourceLoader resourceLoader;
+
+  @PostMapping("/upload")
+  public ObjectNode uploadFile(@RequestParam("file") MultipartFile file) {
     ObjectNode map = mapper.createObjectNode();
-    map.put("msg", "hello world");
+    try {
+      Resource resource = resourceLoader.getResource("classpath:static/");
+      File newFile = new File(resource.getFile(), file.getOriginalFilename());
+      // Copy file to api\target\classes\static\{FileName}
+      FileCopyUtils.copy(file.getBytes(), newFile);
+      map.put("msg", "File uploaded successfully");
+    } catch (Exception e) {
+      map.put("msg", "File upload failed");
+    }
     return map;
   }
 }
--- client/src/components/FileInput.tsx
+++ client/src/components/FileInput.tsx
@@ -5,7 +5,19 @@ export default function FileInput() {
   const [loading, setLoading] = useState<Boolean>(false);
   const handleUpload = (e: FormEvent) => {
     e.preventDefault();
-    fetch("/api/test").then((res) =>
+    setLoading(true);
+    const formData = new FormData();
+    const target: any = e.currentTarget;
+    let files: File[] = target.file.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        formData.append("file", files[i]);
+      }
+    }
+    fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    }).then((res) =>
       res.json().then((jres) => {
         console.log(jres);
         setTimeout(() => {
@@ -18,17 +30,14 @@ export default function FileInput() {
   return (
     <div className="space-y-4">
       {loading && <LoadingBar />}
-      <form
-        onSubmit={(e) => {
-          setLoading(true);
-          handleUpload(e);
-        }}
-      >
+      <form onSubmit={handleUpload}>
         <input
           type="file"
           name="file"
           id="file"
+          accept=".jpg, .jpeg, .png"
           className="file-input file-input-bordered file-input-secondary"
+          required
         />
         <button type="submit" className="btn btn-outline btn-info">
           Compress
